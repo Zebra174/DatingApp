5be5d8ca1120e7e5fa5d59b0ecf683542b06c4dd
Amr Hussam <amrhussam97@gmail.com>
2023-04-23 05:00:11 +0200

    End Of Section 3

---
+++ DatingApp/API/Program.cs
@@ -0,0 +1,34 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<DataContext>(opt =>
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.WithOrigins("https://localhost:4200")
+                          .AllowAnyHeader()
+                          .AllowAnyMethod();
+                      });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseCors(MyAllowSpecificOrigins);
+
+app.MapControllers();
+
+app.Run();
