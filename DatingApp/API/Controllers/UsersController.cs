d0d3eb767ef62e3337dd6a78710ae1362ec89571
Amr Hussam <amrhussam97@gmail.com>
2023-04-28 00:07:51 +0200

    End of section 8

--- DatingApp/API/Controllers/UsersController.cs
+++ DatingApp/API/Controllers/UsersController.cs
@@ -33,8 +33,6 @@ namespace API.Controllers
         public async Task<ActionResult<MemberDto>> GetUser(string username)
         {
            return await _userRepository.GetMemberAsync(username);
-
-           
         }
 
     }
