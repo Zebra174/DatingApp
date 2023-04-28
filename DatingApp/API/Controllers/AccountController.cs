2421461d43e094805ffa8c9a9ab58d6c5353b349
Amr Hussam <amrhussam97@gmail.com>
2023-04-28 08:33:34 +0200

    End of section 11

--- DatingApp/API/Controllers/AccountController.cs
+++ DatingApp/API/Controllers/AccountController.cs
@@ -41,7 +41,8 @@ namespace API.Controllers
             return new UserDto
             {
                 Username = user.Username,
-                Token = _tokenService.CreateToken(user)
+                Token = _tokenService.CreateToken(user),
+               
             };
 
         }
@@ -50,7 +51,7 @@ namespace API.Controllers
         [HttpPost("login")]
         public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
         {
-            var user = await _context.Users.SingleOrDefaultAsync(x =>
+            var user = await _context.Users.Include(p => p.Photos).SingleOrDefaultAsync(x =>
              x.Username == loginDto.Username);
 
             if (user == null) return Unauthorized("Invalid Username");
@@ -67,7 +68,8 @@ namespace API.Controllers
              return new UserDto
             {
                 Username = user.Username,
-                Token = _tokenService.CreateToken(user)
+                Token = _tokenService.CreateToken(user),
+                 PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url
             };
         }
 
