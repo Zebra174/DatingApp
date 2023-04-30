5f1999af14ed4b1f22df815e1ba933229263f0e9
Amr Hussam <amrhussam97@gmail.com>
2023-04-30 06:54:20 +0200

    End OF Section 13

--- DatingApp/client/src/app/_services/members.service.ts
+++ DatingApp/client/src/app/_services/members.service.ts
@@ -1,49 +1,129 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment.development';
 import { Member } from '../_models/member';
-import { map, of } from 'rxjs';
+import { map, of, take } from 'rxjs';
+import { PaginatednResults } from '../_models/pagination';
+import { UserParams } from '../_models/userParams';
+import { AccountService } from './account.service';
+import { User } from '../_models/user';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class MembersService {
-  
-  baseUrl= environment.apiUrl;
-  members: Member[] =[];
 
-  constructor(private http: HttpClient) { }
+  baseUrl = environment.apiUrl;
+  members: Member[] = [];
+  memberCache = new Map();
+  user: User | undefined;
+  userParams: UserParams | undefined
 
-  getMembers(){
-    if(this.members.length>0) return of(this.members);
-    return this.http.get<Member[]> (this.baseUrl+'users').pipe(
-      map(members => {
-        this.members = members;
-        return members;
+
+  constructor(private http: HttpClient,private accountService:AccountService) {
+    this.accountService.currentUser$.pipe(take(1)).subscribe({
+      next: user => {
+        if(user){
+          this.userParams = new UserParams(user);
+          this.user = user;
+        }
+      }
+    })
+   }
+
+   getUserParams(){
+    return this.userParams;
+   }
+   setUserParams(params: UserParams){
+    this.userParams = params;
+   }
+
+   resetUserParams(){
+    if(this.user){
+      this.userParams = new UserParams(this.user);
+      return this.userParams;
+    }
+    return;
+   }
+
+  getMembers(userParams: UserParams) {
+   const response = this.memberCache.get(Object.values(userParams).join('-'));
+   if(response) return of(response);
+
+   let params = this.getPaginationHeader(userParams.pageNumber, userParams.pageSize);
+    
+    params= params.append('minAge',userParams.minAge);
+    params= params.append('maxAge',userParams.maxAge); 
+    params= params.append('gender',userParams.gender);
+    params= params.append('orderBy',userParams.orderBy);
+
+    return this.getPaginatedResults<Member[]>(this.baseUrl + 'users',params).pipe(
+      map(response => {
+        this.memberCache.set(Object.values(userParams).join('-'), response);
+        return response;
       })
-    );
+    )
   }
 
-  getMember(username:string){
-    const member = this.members.find(x => x.username == username);
-    if(member) return of(member);
-    return this.http.get<Member>(this.baseUrl+'users/'+username);
+ 
+  getMember(username: string) {
+   
+    const member = [...this.memberCache.values()]
+   .reduce((arr,elem) => arr.concat(elem.result), [])
+   .find((member:Member) => member.username === username);
+
+   
+   if(member) return of(member);
+    
+   return this.http.get<Member>(this.baseUrl + 'users/' + username);
   }
 
-  updateMember(member:Member) {
-    return this.http.put(this.baseUrl+'users', member).pipe(
+  
+  updateMember(member: Member) {
+    return this.http.put(this.baseUrl + 'users', member).pipe(
       map(() => {
         const index = this.members.indexOf(member);
-        this.members[index] = {...this.members[index], ...member}
+        this.members[index] = { ...this.members[index], ...member }
       })
     );
   }
 
-  setMainPhotos(photoId:number){
-    return this.http.put(this.baseUrl+'users/set-main-photo/'+photoId,{});
+  setMainPhotos(photoId: number) {
+    return this.http.put(this.baseUrl + 'users/set-main-photo/' + photoId, {});
   }
 
-  deletePhoto(photoId:number){
-    return this.http.delete(this.baseUrl+'users/delete-photo/'+photoId);
+  deletePhoto(photoId: number) {
+    return this.http.delete(this.baseUrl + 'users/delete-photo/' + photoId);
   }
+
+   
+
+  private getPaginatedResults<T>(url:string,params: HttpParams) {
+    const paginatedResult: PaginatednResults<T> = new PaginatednResults<T>;
+ 
+     return this.http.get<T>(url, { observe: 'response', params }).pipe(
+       map(response => {
+         if (response.body) {
+           paginatedResult.result = response.body;
+         }
+         const pagination = response.headers.get('Pagination');
+         if (pagination) {
+           paginatedResult.pagination = JSON.parse(pagination);
+         }
+         return paginatedResult;
+       })
+     );
+   }
+ 
+   
+   private getPaginationHeader(pageNumber: number, pageSize: number) {
+     let params = new HttpParams();
+ 
+     params = params.append('pageNumber', pageNumber);
+     params = params.append('pageSize', pageSize);
+    
+     return params;
+   }
+ 
 }
